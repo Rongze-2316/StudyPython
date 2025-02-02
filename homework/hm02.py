aa4e93e55e1c494dc50830f0bf911fa94dbffa09
Rongze-2316 <331262086@qq.com>
2025-02-02 15:06:36 +0800

    Add files via upload

---
+++ homework/hm02.py
@@ -0,0 +1,10 @@
+print("请输入数字")
+M = int(input())
+for i in range(1,M+1):
+	print("进入第%d次循环"%(i))
+	print("第%d次循环是%d"%(i,i))
+	print("结束第%d次循环"%(i))
+
+#range(1,M+1) = [1,2,3,……,M]
+#range(0,1) = [0]
+#range(0,0) = null;
---
+++ homework/while.py
@@ -0,0 +1,35 @@
+#print("请输入数字")
+#M = int(input())
+
+i = 1
+print(a1)
+a0,a1=0,1
+a2=a0+a1
+while i < 21:
+	a2=a0+a1 
+	print(a2)
+	a0=a1
+	a1=a2
+	i = i + 1
+
+
+
+
+'''
+#斐波那契数列
+#1,1,2,3,5,8,13
+#输出前20个
+
+a2 = a0 + a1
+a(n+1) = a(n-1) + an
+
+print(1)
+a0,a1 = 0,1
+a2 = a0 + a1
+while() 	
+	a2 = a0 + a1
+	print(a2)
+	a0 = a1
+	a1 = a2
+	i = i + 1
+'''
